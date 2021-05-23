1476d281aaeeb2dab1f1a8f38adfed6742b8792d
SergiyStepanchuk <prochliste@gmail.com>
2021-05-23 12:29:17 +0300

    init

---
+++ OOP_Lab_7.1/UnitTest/UnitTest.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			array<int, 100> arr = { 2, 3, 2, 5, 2 };
+			Assert::AreEqual(avg(arr, 5), 2.);
+		}
+	};
+}
